30f9346f6fa58c58d7b39290386b91fc4f7a240b
Vaibhav Nagnath Shinde <103619246+Vaibhu18@users.noreply.github.com>
2024-01-30 11:12:21 +0000

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,22 @@
-const express = require("express");
-const app = express();
+let express = require("express");
+let app = express();
+let port = 3000;
 
-app.get('/',(req, res)=>{
-    res.send("Vaibhav Shinde hey");
-}).listen(3000);
+app.set("view engine","ejs");
+
+app.get('/', (req, res)=>{
+   
+    console.log(`Server is started on ${port} and reqest params is ${name.a}`);
+})
+
+app.get('/home',(req, res)=>{
+    res.render("index");
+    console.log(`Server is started on ${port} and i am from home`);
+})
+
+app.get('/error',(req, res)=>{
+    res.render("error");
+    console.log(`Server is started on ${port} and i am from home`);
+})
+
+app.listen(port);
